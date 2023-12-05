4ce73b1fe03e172d59f1ed4c869a9305a3c53dfd
Jayani <jayanipatel23@gmail.com>
2023-12-05 09:41:43 +0000

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,4 +1,6 @@
 # Main
+# Note : Need to create individual key for evry different agents.
+
 from AutonomousElectricVehicle import Vehicle
 from LocalNavigationAgent import LocalNavigationAgent
 from CentralizedControlAgent import CentralizedControlAgent
@@ -16,13 +18,13 @@ ae2 = Vehicle(vehicle_id=2)
 
 # Test CentralizedControlAgent
 #===================================================================================================================
-centralizedControl_agent = CentralizedControlAgent([ae1, ae2],"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")
+centralizedControl_agent = CentralizedControlAgent([ae1, ae2],"openAI-KEY")
 centralizedControl_agent.plan_route_for_vehicle(ae1, "London","shortest path")
 centralizedControl_agent.allocate_resources()
 
 # Test LocalNavigationAgent
 #==================================================================================================================
-localNavigation_agent = LocalNavigationAgent(ae1, "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")
+localNavigation_agent = LocalNavigationAgent(ae1, "openAI-KEY")
 
 # Set initial vehicle location and destination
 ae1.update_location("Oxford")
@@ -31,8 +33,8 @@ localNavigation_agent.navigate()
 
 # Test CommunicationAgent
 #============================================================================================================
-communication_agent1 = CommunicationAgent(ae1, "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")
-communication_agent2 = CommunicationAgent(ae2, "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")
+communication_agent1 = CommunicationAgent(ae1, "openAI-KEY")
+communication_agent2 = CommunicationAgent(ae2, "openAI-KEY")
 
 # Update neighbors for communication
 communication_agent1.update_neighbors([ae1])
@@ -47,30 +49,30 @@ communication_agent2.share_traffic_info()
 charging_station1 = ChargingStation(station_id=1)
 charging_station2 = ChargingStation(station_id=2)
 
-energy_agent1 = EnergyManagementAgent(ae1, [charging_station1, charging_station2], "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")
+energy_agent1 = EnergyManagementAgent(ae1, [charging_station1, charging_station2], "openAI-KEY")
 
 # Perform energy management tasks
 energy_agent1.optimize_charging_schedule()
 
 # Test SafetyMonitoringAgent
 #=======================================================================================================================
-safetyMonitoring_agent = SafetyMonitoringAgent(ae1, "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")
+safetyMonitoring_agent = SafetyMonitoringAgent(ae1, "openAI-KEY")
 safetyMonitoring_agent.monitor_safety()
 
 # Test MaintenanceAgent
 #========================================================================================================================
-maintenance_agent = MaintenanceAgent(ae1, "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")
+maintenance_agent = MaintenanceAgent(ae1, "openAI-KEY")
 maintenance_agent.monitor_vehicle_health()
 maintenance_agent.schedule_maintenance()
 
 # Test HelpDeskAgent
 #==================================================================================================================
-helpdesk_agent = HelpDeskAgent("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")
+helpdesk_agent = HelpDeskAgent("openAI-KEY")
 helpdesk_agent.handle_user_query(ae1, "What is the charging status of my vehicle?")
 
 # Test UserAgent
 #===================================================================================================================
-user_agent = UserAgent(1, "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")
+user_agent = UserAgent(1, "openAI-KEY")
 user_agent.request_ride("Oxford","London")
 user_agent.ask_question("How can I optimize my route?")
 
