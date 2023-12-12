6d773f8107fa698401aa88853cd5e56f6fa4486c
Jayani <jayanipatel23@gmail.com>
2023-12-12 20:58:31 +0000

    Update CentralizedControlAgent.py

--- CentralizedControlAgent.py
+++ CentralizedControlAgent.py
@@ -6,11 +6,11 @@ class CentralizedControlAgent:
         self.api_key = api_key
         openai.api_key = self.api_key
 
-    def plan_route_for_vehicle(self, vehicle, destination, preferences):
+    def plan_route_for_vehicle(self, vehicle, current_location, destination, preferences):
         """
         Plans a route for a specific vehicle based on the destination and user preferences.
         """
-        prompt = f"Generate a route for vehicle {vehicle.vehicle_id} to {destination} with preferences: {preferences}."
+        prompt = f"Generate a route for vehicle {vehicle.vehicle_id} from {current_location} to {destination} with preferences: {preferences}."
         response = openai.ChatCompletion.create(model="gpt-4",
                                                 messages=[{'role': 'assistant', 'content': prompt}],
                                                 max_tokens=3000)
@@ -26,6 +26,18 @@ class CentralizedControlAgent:
             if vehicle.route is not None:
                 resource_allocation = self.allocate_resources_for_vehicle(vehicle)
                 print(f"Vehicle {vehicle.vehicle_id}: Resources allocated - {resource_allocation}")
+
+    def allocate_resources_for_vehicle(self, vehicle):
+        """
+        Allocates resources for a specific vehicle based on its planned route.
+        """
+        prompt = f"Allocate resources for vehicle {vehicle.vehicle_id} with route: {vehicle.route}."
+        response = openai.ChatCompletion.create(model="gpt-4",
+                                                messages=[{'role': 'system', 'content': prompt}],
+                                                max_tokens=1500)
+        resource_allocation = response["choices"][0]["message"]["content"]
+        return resource_allocation.strip()
+
 
     def allocate_resources_for_vehicle(self, vehicle):
         """
