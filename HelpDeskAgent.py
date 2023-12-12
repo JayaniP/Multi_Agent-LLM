96b3dda33c8b75ec18903d2d49c271788c89eba1
Jayani <jayanipatel23@gmail.com>
2023-12-12 21:59:24 +0000

    Update HelpDeskAgent.py

--- HelpDeskAgent.py
+++ HelpDeskAgent.py
@@ -1,18 +1,1 @@
-import openai
-
-class HelpDeskAgent:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        openai.api_key = self.api_key
-
-    def handle_user_query(self, vehicle_id, user_query):
-        """
-        Handles user queries and provides relevant information using the Large Language Model.
-        """
-        prompt = f"Handle user query for Vehicle {vehicle_id}: {user_query}."
-        response = openai.ChatCompletion.create(model="gpt-4",
-                                                messages=[{'role': 'assistant', 'content': prompt}],
-                                                max_tokens=2000)
-        query_response = response["choices"][0]["message"]["content"]
-        print(f"Received Response - {query_response.strip()}")
 
